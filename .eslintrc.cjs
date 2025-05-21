be1ca6c8f5c22d7f12ae301e1d3c831ddbfa08fb
fr0gydev <ljh130334@naver.com>
2025-05-21 17:23:07 +0900

    project initial setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off", // React 17+ JSX 변환 자동
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "no-console": "warn",
+  },
+};
