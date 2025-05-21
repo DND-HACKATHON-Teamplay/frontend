4f25572ab8e4faf56f0f4911cecc69804b4fa606
fr0gydev <ljh130334@naver.com>
2025-05-21 18:02:54 +0900

    feat: 프로젝트 폴더 구조 세팅 및 Login 페이지 추가

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+// 구글 소셜 로그인 처리를 위한 로그인 페이지입니다.
+// 나중에 OAuth2 인증 및 리다이렉션 처리를 추가합니다.
+
+const Login = () => {
+  return (
+    <div>
+      {/* 구글 로그인 버튼 위치 */}
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
