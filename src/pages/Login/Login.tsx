62d665f2b0ec0e987ffdb352642de41b5354d430
fr0gydev <ljh130334@naver.com>
2025-05-24 06:27:40 +0900

    feat: login ui 구현

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,6 +4,7 @@ import { oauthAPI } from '../../services/Login/auth';
 import { tokenUtils } from '../../utils/auth';
 import styles from './Login.module.css';
 import loginGif from '../../assets/login/login.gif';
+import logo from '../../assets/login/logo.svg';
 import googleIcon from '../../assets/login/google.svg';
 import kakaoIcon from '../../assets/login/kakao.svg';
 
@@ -11,7 +12,6 @@ const Login = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // 이미 로그인된 사용자는 홈으로 리다이렉트
     if (tokenUtils.isLoggedIn()) {
       navigate('/');
     }
@@ -30,6 +30,14 @@ const Login = () => {
       <div className={styles.content}>
         <div className={styles.logoSection}>
           <img src={loginGif} alt="똑똑" className={styles.loginGif} />
+        </div>
+
+        <div className={styles.descriptionSection}>
+          <img src={logo} alt="똑똑!" className={styles.logoImage} />
+          <span className={styles.descriptionText}>
+            하루 한 번, 어르신 마음을 <strong>똑똑</strong> 두드리는 <br />
+            AI 케어콜 서비스
+          </span>
         </div>
 
         <div className={styles.buttonSection}>
